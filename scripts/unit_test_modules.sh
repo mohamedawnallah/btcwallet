94567d9250bd08ec0aedb82bdf5930e3945131e7
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2025-08-20 23:48:57 +0000

    scripts: add unit test modules

---
+++ scripts/unit_test_modules.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+IGNORE="tools"
+SUBMODULES=$(find . -mindepth 2 -name "go.mod" | cut -d'/' -f2 | grep -v "$IGNORE")
+
+for submodule in $SUBMODULES
+do
+  pushd $submodule
+
+  echo "Running submodule unit tests in $(pwd)"
+  echo "testing $submodule..."
+  go test -timeout=5m
+  
+  if [[ "$submodule" == "kvdb" ]]
+  then
+      echo "testing $submodule with sqlite..."
+      go test -tags="kvdb_sqlite" -timeout=5m
+      
+      echo "testing $submodule with postgres..."
+      go test -tags="kvdb_postgres" -timeout=5m
+      
+      echo "testing $submodule with etcd..."
+      go test -tags="kvdb_etcd" -timeout=5m
+  fi
+  
+  popd
+done
